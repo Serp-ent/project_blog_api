09e046257c14f50a809c4cf9ae7dbd29c26fe0cc
kenny <kenny909@mat.umk.pl>
2024-08-09 22:32:04 +0200

    Add api for posts

--- backend_api/routes/postsRoute.js
+++ backend_api/routes/postsRoute.js
@@ -2,51 +2,141 @@
 const { Router } = require('express');
 const postsRoute = Router();
 const passport = require('passport');
+const prisma = require('../db/prismaClient');
 
-postsRoute.use(
-  (req, res, next) => {
-    console.log('authentication in postsRoute');
-    next();
-  },
-  passport.authenticate('jwt', { session: false }),
-  (req, res, next) => {
-    console.log('authenticated in postsRoute');
-    next();
+// TODO: add authentication
 
-  },
-);
-
-
-postsRoute.get('/', (req, res) => {
-  res.send('Get all posts');
+postsRoute.get('/', async (req, res) => {
+  try {
+    const posts = await prisma.post.findMany({});
+    return res.json({ posts });
+  } catch (err) {
+    res.status(404).json({ error: "Cannot fetch posts" });
+  }
 });
 
-postsRoute.post('/', (req, res) => {
-  res.send('create new post');
+postsRoute.post('/', async (req, res) => {
+  // TODO: get info from req.users 
+  const { title, content } = req.body;
+  const authorId = req.user.id;
+
+  try {
+    const post = await prisma.post.create({
+      data: {
+        authorId,
+        title,
+        content,
+      }
+    });
+
+    res.json({ message: "success", post });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ error: "Failed to create post" });
+  }
 });
 
-postsRoute.get('/:id', (req, res) => {
-  res.send(`get post with id ${req.params.id}`);
+postsRoute.get('/:id', async (req, res) => {
+  const id = Number(req.params.id);
+  try {
+    const post = await prisma.post.findUnique({
+      where: { id }
+    });
+
+    if (!post) {
+      return res.status(404).json({ error: `Post with id ${id} not found` });
+    }
+
+    return res.json({ message: "success", post });
+  } catch (err) {
+    return res.status(400).json({ error: 'Internal server error' });
+  }
 });
 
-postsRoute.delete('/:id', (req, res) => {
-  res.send(`delete post with id ${req.params.id}`);
+postsRoute.delete('/:id', async (req, res) => {
+  const id = Number(req.params.id);
+  try {
+    await prisma.post.delete({
+      where: { id }
+    });
+
+    return res.json({ message: 'post deleted' });
+  } catch (err) {
+    if (err.code === 'P2025') {
+      return res.json({ message: 'post deleted' });
+    }
+
+    return res.status(400).json({ error: 'Internal server error' });
+  }
 });
 
-postsRoute.post('/:id', (req, res) => {
-  res.send(`update post with id ${req.params.id}`);
+postsRoute.post('/:id', async (req, res) => {
+  // TODO: add query parameter to hide post
+  const id = Number(req.params.id);
+  try {
+    const post = await prisma.post.update({
+      where: { id },
+      data: {
+        published: true,
+      }
+    });
+
+    const visibility = post.published ? 'published' : 'hidden';
+    return res.json({ message: `post ${id} is now ${visibility}` });
+  } catch (err) {
+    if (err.code === 'P2025') {
+      return res.json({ message: `Post with id ${id} not found` });
+    }
+    return res.json({ error: "Internal server error" });
+  }
 });
 
-postsRoute.post('/:id', (req, res) => {
+postsRoute.put('/:id', (req, res) => {
+  // TODO: 
   res.send('switch with new version of post');
 });
 
-postsRoute.get('/:id/comments', (req, res) => {
-  res.send(`get all comments for post ${req.params.id}`);
+postsRoute.get('/:id/comments', async (req, res) => {
+  const postId = Number(req.params.id);
+  try {
+    // Does post like this exists?
+    const exist = await prisma.post.findUnique({
+      where: { id: postId },
+    });
+    if (!exist) {
+      return res.status(404).json({ error: "No post with given id" });
+    }
+
+    const comments = await prisma.comment.findMany({
+      where: { postId }
+    });
+
+    return res.json({ comments })
+  } catch (err) {
+    res.status(400).json({ error: "Internal server error" });
+  }
 });
 
-postsRoute.post('/:id/comments', (req, res) => {
-  res.send(`post new comment for post ${req.params.id}`);
+
+postsRoute.post('/:id/comments', async (req, res) => {
+  // TODO: add validation
+  const { content } = req.body;
+  const postId = Number(req.params.id);
+  // const authorId = req.user.id;
+  const authorId = 8;
+  try {
+    const comment = await prisma.comment.create({
+      data: {
+        authorId,
+        postId,
+        content,
+      }
+    });
+    res.json({ message: 'New comment created', comment });
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({ error: "Failed to post a comment" })
+  }
 });
 
 module.exports = postsRoute;
