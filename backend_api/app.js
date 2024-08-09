07c5d596743e9ff01ec91bd38e90b30d3e71162e
kenny <kenny909@mat.umk.pl>
2024-08-09 14:04:50 +0200

    Prepare express app

---
+++ backend_api/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello world');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log('Express app listening on port', PORT);
+});
