b5425c1cb84f618f46252401a1aaf38a2ecca43f
kenny <kenny909@mat.umk.pl>
2024-08-10 20:52:22 +0200

    Fix labels for signin

--- reader/src/components/SigninForm.jsx
+++ reader/src/components/SigninForm.jsx
@@ -54,24 +54,26 @@ export default function SigninForm() {
       onSubmit={handleSubmit}
     >
       <div className={styles.formField}>
-        <label>
+        <label htmlFor="username">
           Username:
         </label>
         <input
           type="text"
           name="username"
+          id="username"
           value={username}
           onChange={e => setUsername(e.target.value)} />
       </div>
 
 
       <div className={styles.formField}>
-        <label>
+        <label htmlFor="password">
           Password:
         </label>
         <input
           type="password"
           name="password"
+          id="password"
           value={password}
           onChange={e => setPassword(e.target.value)} />
       </div>
