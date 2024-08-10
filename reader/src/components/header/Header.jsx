255689ff20ffd0eb7a2b9a8b284774737937d837
kenny <kenny909@mat.umk.pl>
2024-08-10 12:07:19 +0200

    Simple structure for header

--- reader/src/components/header/Header.jsx
+++ reader/src/components/header/Header.jsx
@@ -3,7 +3,24 @@ import styles from "./Header.module.css";
 export default function Header() {
   return (
     <header className={styles.header}>
-      <p>Mras</p>
-    </header>
+      <div className={styles.logo}>
+        Logo
+      </div>
+      <nav className={styles.nav}>
+        <ul className={styles.navItems}>
+          {/* TODO: create some list item */}
+          <li>Home</li>
+          <li>Posts</li>
+          <li>Contact</li>
+          <li>About</li>
+        </ul>
+        <div className={styles.searchBar}>
+          <label>
+            Search
+            <input />
+          </label>
+        </div>
+      </nav>
+    </header >
   );
 }
