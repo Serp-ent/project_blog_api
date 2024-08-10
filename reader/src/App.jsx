8840eb532d3d879e23b8425450801fc575044918
kenny <kenny909@mat.umk.pl>
2024-08-10 11:57:35 +0200

    Initial commit for reader frontend

---
+++ reader/src/App.jsx
@@ -0,0 +1,33 @@
+import Footer from "./components/footer/Footer"
+import Header from "./components/header/Header"
+
+{
+  /*
+  TODO:
+    header
+    footer
+    homePage
+    BlogPostList
+    BlogPostCard
+    BlogPostPage
+    SideBar
+    SearchResultPage
+    AboutPage
+    CommentSection
+    Comment
+    Pagination or InfiniteScroll
+    LoadingSpinner
+    ErrorBoundary
+    NotFoundPage */
+}
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  )
+}
+
+export default App
