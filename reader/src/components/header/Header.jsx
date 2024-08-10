25984ffa4011d23d7bdec0d4b4a620c9b37c4b7b
kenny <kenny909@mat.umk.pl>
2024-08-10 20:13:55 +0200

    Finish blog header for small devices

--- reader/src/components/header/Header.jsx
+++ reader/src/components/header/Header.jsx
@@ -8,40 +8,35 @@ export default function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
-        Logo
+        Awesome Blog
       </div>
+
       <nav className={styles.nav}>
         <ul className={styles.navItems}>
-          {/* TODO: create some list item */}
-          <li>
-            <Link to={"home"}>Home</Link>
-          </li>
-          <li>
-            <Link to={"authors"}>Authors</Link>
-          </li>
-          <li>
-            <Link to={"authors"}>About</Link>
-          </li>
+          <Link to={"home"}>
+            <li>Home</li>
+          </Link>
+          <Link to={"authors"}>
+            <li>Authors</li>
+          </Link>
+          <Link to={"authors"}>
+            <li>About</li>
+          </Link>
         </ul>
-        <div className={styles.searchBar}>
-          <label>
-            Search
-            <input />
-          </label>
+
+        <div className={isAuthenticated ? styles.logoutButton : styles.accountSection}>
+          {
+            isAuthenticated ? (
+              <button onClick={logout}>Log out</button>
+            ) : (
+              <>
+                <Link to={'signin'}>Sign in</Link>
+                <Link to={'signup'}>Sign up</Link>
+              </>
+            )
+          }
         </div>
       </nav>
-      <div>
-        {
-          isAuthenticated ? (
-            <button onClick={logout}>Log out</button>
-          ) : (
-            <>
-              <Link to={'signin'}>Sign in</Link>
-              <Link to={'signup'}>Sign up</Link>
-            </>
-          )
-        }
-      </div>
     </header >
   );
 }
