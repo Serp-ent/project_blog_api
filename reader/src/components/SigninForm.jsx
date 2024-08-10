6f1c65c3cc22a0f7992174664756700f646dca99
kenny <kenny909@mat.umk.pl>
2024-08-10 13:23:25 +0200

    Receive token from signin

--- reader/src/components/SigninForm.jsx
+++ reader/src/components/SigninForm.jsx
@@ -1,5 +1,68 @@
+import { useState } from "react";
+
 export default function SigninForm() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    setLoading(true);
+    setError("");
+
+    try {
+      const response = await fetch('http://localhost:3000/api/users/login', {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name: username, password })
+      });
+
+      if (!response.ok) {
+        throw new Error('Login failed');
+      }
+
+      const result = await response.json();
+      const { token } = result;
+      if (!token) {
+        throw new Error('Token not received');
+      }
+
+      localStorage.setItem('authToken', token);
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <h1>Sign in Form</h1>
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input
+          type="text"
+          name="username"
+          value={username}
+          onChange={e => setUsername(e.target.value)} />
+      </label>
+      <label>
+        Password:
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={e => setPassword(e.target.value)} />
+      </label>
+
+      <button type="submit" disabled={loading}>
+        {loading ? 'Signing in...' : 'Sign in'}
+      </button>
+
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+    </form>
   );
 }
