ea7b43af40c20faf2a08ebe501de8d33f97ac2e0
kenny <kenny909@mat.umk.pl>
2024-08-11 11:10:30 +0200

    Pretty print date in comment section

--- reader/src/components/comment/Comment.jsx
+++ reader/src/components/comment/Comment.jsx
@@ -1,9 +1,12 @@
 import styles from "./Comment.module.css";
 
 export default function Comment({ comment: c }) {
+  const date = new Date(c.createdAt);
+  const prettyDate = `${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')} ${date.getDate().toString().padStart(2, '0')}/${(date.getMonth() + 1).toString().padStart(2, '0')}/${date.getFullYear().toString().slice(-2)}`;
+
   return (
     <div className={styles.comment}>
-      <h6>{c.author.username} at {c.createdAt}</h6>
+      <h6>{c.author.username} at {prettyDate}</h6>
       <p>{c.content}</p>
     </div>
   );
